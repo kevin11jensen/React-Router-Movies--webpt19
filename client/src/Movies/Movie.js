e5623b21e808eb73d653ce527b544e2e8c904d07
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-08-03 09:03:36 -0700

    movie details not showing up

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -14,17 +14,17 @@ const Movie = (props) => {
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+        axios
+          .get(`http://localhost:5000/api/movies/${id}`)
+          .then(response => {
+            setMovie(response.data);
+          })
+          .catch(error => {
+            console.error(error);
+          });
 
-  },[]);
-  
+      },[]);
+    
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => {
   // }
